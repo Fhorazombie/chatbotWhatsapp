37b7d44970d8cdeae6a3535a851270918d69ef40
YoshZombie <fhorandremon@gmail.com>
2025-06-28 14:59:22 +0000

    conexion

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("baileys");
+const QRCode = require("qrcode")
+
+async function conectarWhatsapp(){
+
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
+
+    // config
+    const sock = makeWASocket({
+        auth: state
+    });
+
+    sock.ev.on("creds.update", saveCreds);
+
+    // conexion
+    sock.ev.on("connection.update", async(update) => {
+        const { connection, lastDisconnect, qr } = update;
+
+        if(connection === 'close'){
+            const puedeConectarse = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+            if(puedeConectarse){
+                conectarWhatsapp()
+            }
+        }else if(connection === 'open'){
+            console.log("CONEXION ABIERTA!!!");
+        }
+
+        if(qr){
+            console.log(await QRCode.toString(qr, {type: 'terminal', small: true}));
+        }
+    })
+
+}
+
+conectarWhatsapp()
